e9497280dc76ee89dd6f4aba7554deec0dcde65e
abhit2109 <122540059+abhit2109@users.noreply.github.com>
2023-01-18 08:38:48 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,70 @@
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    . # # # .
+    . # . # .
+    . # . # .
+    `)
+basic.clearScreen()
+basic.pause(1000)
+basic.showLeds(`
+    . # . . .
+    . # . . .
+    . # # # .
+    . # . # .
+    . # # # .
+    `)
+basic.clearScreen()
+basic.pause(1000)
+basic.showLeds(`
+    . # . . .
+    . # . . .
+    . # # # .
+    . # . # .
+    . # . # .
+    `)
+basic.clearScreen()
+basic.pause(1000)
+basic.showLeds(`
+    . . # . .
+    . . . . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.clearScreen()
+basic.pause(1000)
+basic.showLeds(`
+    . # # # .
+    . . . # .
+    . # # # .
+    . # . . .
+    . # # # .
+    `)
+basic.clearScreen()
+basic.pause(1000)
+basic.showLeds(`
+    . . # . .
+    . # # . .
+    . . # . .
+    . . # . .
+    . # # # .
+    `)
+basic.clearScreen()
+basic.pause(1000)
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    . # . # .
+    . # . # .
+    . . # . .
+    `)
+basic.clearScreen()
+basic.pause(1000)
+basic.showLeds(`
+    . # # # .
+    . # . # .
+    . # # # .
+    . . . # .
+    . # # # .
+    `)
